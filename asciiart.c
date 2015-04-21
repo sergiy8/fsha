d9fc252709ce717e02918b5234de69367d468eae
Sergiy Gulchuk <sgulchuk@jawbone.com>
2015-04-22 01:44:55 +0300

    asciiart pen,color,facecontrol

--- asciiart.c
+++ asciiart.c
@@ -4,10 +4,20 @@
 #include <ncurses.h>
 
 #include "sha.h"
+#include "facecontrol.h"
 #include "pack.h"
+#include "getarg.h"
 
+enum { COLOR_SUCCESS=1, COLOR_FAIL, COLOR_DRAW};
 
-static WINDOW * wdoska, *whex, *wlog, *wask, *whelp;
+#define remove_bit(x,n)   x = ((x & ~ALLONE((n)+1)) >> 1) | (x & ALLONE(n))
+#define insert_bit(x,n,b) x = ((x & ~ALLONE(n))<<1) | (!!(b) << (n)) | (x & ALLONE(n))
+
+static char pens[] = "wbWB";
+static int pen = 0;
+static char * pens_prompt = "Pen:";
+
+static WINDOW * wdoska, *whex, *wlog, *wask, *whelp, *wpen;
 #define putlog(fmt,args...) do{wprintw(wlog,fmt"\n",##args); wrefresh(wlog);}while(0)
 
 #if 0
@@ -21,20 +31,48 @@ uint32_t d = 0;
 
 #define DOSKAY ((LINES-8)/2)
 #define DOSKAX 4
+#define DOSKASY 8
+#define DOSKASX 12
 
-#define HEXY (DOSKAY+8)
-#define HEXX (DOSKAX + 14)
+#define HEXY (DOSKAY + DOSKASY - 1)
+#define HEXX (DOSKAX + DOSKASX + 8)
+#define HEXSY 1
+#define HEXSX 20
 
 #define LOGY (HEXY+2)
 #define LOGX 0
+#define LOGSY (LINES-LOGY)
+#define LOGSX COLS
 
 #define ASKY DOSKAY
-#define ASKX (DOSKAX+20)
+#define ASKX HEXX
+#define ASKSY 3
+#define ASKSX 20
 
 #define HELPY 1
 #define HELPX 1
+#define HELPSY 10
+#define HELPSX 40
+
+#define PENY (DOSKAY-2)
+#define PENX 1
+#define PENSY 1
+#define PENSX (__builtin_strlen(pens_prompt) + __builtin_strlen(pens))
 
 #include "megask.inc"
+
+static void draw_pen(void){
+	int i;
+	mvwprintw(wpen,0,0,"%s",pens_prompt);
+	for(i=0;pens[i];i++) {
+		if(i==pen)
+			wattron(wpen,A_REVERSE);
+		mvwprintw(wpen,0,i+__builtin_strlen(pens_prompt),"%c",pens[i]);
+		if(i==pen)
+			wattroff(wpen,A_REVERSE);
+	}
+	wrefresh(wpen);
+}
 
 static void doska(uint32_t w, uint32_t b, uint32_t d){
 	int i,j;
@@ -50,47 +88,101 @@ static void doska(uint32_t w, uint32_t b, uint32_t d){
 	for(i=0;i<8;i++)
 	for(j=0;j<4;j++) {
 		int k = (28-4*i) + j;
-		mvwprintw(wdoska,i, j*3 + 1 + (i&1?0:2),"%c", pole[k]?:'.');
+		mvwprintw(wdoska,i, j*3 + (i&1?0:2),"%c", pole[k]?:'.');
 	}
 	wrefresh(wdoska);
 }
 
+const char * summary(void){
+	int value = megask(b,w,d);
+	int arank = _popc(b);
+	if (_popc(b) ==0 ||
+		_popc(b) > 24 ||
+		_popc(w) > 12 ||
+		_popc(w^ALLONE(arank)) > 12
+	)
+		return "IllegalCount";
+	if (FaceControl(b,w,d))
+		return "IllegalDamka";
+	switch(value) {
+		default: return "InternalError";
+		case 0:
+			if(arank==8)
+				return "Draw?";
+			else {
+				wattron(wask,COLOR_PAIR(COLOR_YELLOW));
+				return "Draw";
+			}
+		case 1:
+			wattron(wask,COLOR_PAIR(COLOR_SUCCESS));
+			return "WhiteWin";
+		case 2:
+			wattron(wask,COLOR_PAIR(COLOR_FAIL));
+			return "BlackWin";
+		case 3:
+			wattron(wask,COLOR_PAIR(COLOR_YELLOW));
+			return "DrawAfterTakege";
+		case 4:
+			return "NotInBase";
+	}
+}
 
-int main(){
+int main(int argc, char ** argv){
+	if (argc == 4 ) {
+		b = getarg(1);
+		w = getarg(2);
+		d = getarg(3);
+	} else if (argc!=1) {
+		error("Three args!");
+	}
 	setlocale(LC_ALL,"");
 	initscr();
+
+	start_color();
+	init_pair(COLOR_SUCCESS,COLOR_GREEN,COLOR_BLACK);
+	init_pair(COLOR_FAIL,COLOR_RED,COLOR_BLACK);
+	init_pair(COLOR_DRAW,COLOR_YELLOW,COLOR_BLACK);
+
 	mousemask(ALL_MOUSE_EVENTS, NULL);
 	cbreak();
 
-	wdoska = newwin(8,13,DOSKAY,DOSKAX);
+	wdoska = newwin(DOSKASY,DOSKASX,DOSKAY,DOSKAX);
 	keypad(wdoska,TRUE);
 
-	whex = newwin(1,COLS-HEXX,HEXY,HEXX);
+	whex = newwin(HEXSY,HEXSX,HEXY,HEXX);
 	keypad(whex,TRUE);
 //	idcok(whex,TRUE);
 //	immedok(whex,TRUE);
 
-	wlog = newwin(LINES-LOGY,COLS-LOGX,LOGY,LOGX);
+	wlog = newwin(LOGSY,LOGSX,LOGY,LOGX);
 	scrollok(wlog,TRUE);
 	idlok(wlog,TRUE);
 
-	wask = newwin(2,20,ASKY,ASKX);
+	wask = newwin(ASKSY,ASKSX,ASKY,ASKX);
 
-	whelp = newwin(5,COLS-HELPX,HELPY,HELPX);
+	whelp = newwin(HELPSY,HELPSX,HELPY,HELPX);
 	mvwprintw(whelp,0,0,"TAB - rotate");
 	mvwprintw(whelp,1,0,"Shift/Arrows - shift position");
 	mvwprintw(whelp,2,0,"Shift/Digit - change pawn");
 	wrefresh(whelp);
+
+
+	wpen = newwin(PENSY,PENSX,PENY,PENX);
+	draw_pen();
 
 	megask_init();
 
 new_doska:
 	doska(w,b,d);
 
+	wattrset(wask,0);
 	mvwprintw(wask, 0, 0, "Megask: %d", megask(b,w,d));
+	mvwprintw(wask, 1, 0, "Arank : %d-%d", _popc(b),_popc(w));
+	mvwprintw(wask, 2, 0, "%s",summary());
+	wclrtobot(wask);
 	wrefresh(wask);
 
-	char str[COLS];
+	char str[HEXSX];
 	int pos=0;
 	snprintf(str,sizeof(str),"%08X %X %X          ",b,w,d);
 	for(;;) {
@@ -104,7 +196,7 @@ new_doska:
 				  char * p = strchr(klava,c);
 				  if(p!=NULL) {
 						int invert = p - klava;
-						if (invert < __builtin_popcount(b)) {
+						if (invert < _popc(b)) {
 							w ^= 1<<invert;
 							goto new_doska;
 						}
@@ -125,16 +217,6 @@ new_doska:
 				if(str[pos]==0)
 					continue;
 				pos++;
-				continue;
-			case 0x107: // Del
-				if(pos==0)
-					continue;
-				pos--;
-				{ int i;
-				  for(i=pos;str[i+1];i++)
-					str[i]=str[i+1];
-				  str[i]=0;
-				}
 				continue;
 			case 0x0a:
 				wclrtobot(whex);
@@ -174,15 +256,40 @@ new_doska:
 				  ub = _brev(ub);
 				  ud = _brev(ud);
 				  Pack(&b,&w,&d,uw,ub,ud);
-				  w ^= ALLONE(__builtin_popcount(b));
+				  w ^= ALLONE(_popc(b));
 				}
 				goto new_doska;
 			case KEY_MOUSE:
 				if(getmouse(&event) != OK) continue;
-				putlog("bstate=%x",event.bstate);
 				if(!(event.bstate & NCURSES_BUTTON_CLICKED)) continue;
+				if (wmouse_trafo(wpen, &event.y, &event.x, FALSE)){
+					if(event.x < __builtin_strlen(pens_prompt))
+						continue;
+					pen = event.x - __builtin_strlen(pens_prompt);
+					draw_pen();
+					continue;
+				}
 				if (!wmouse_trafo(wdoska, &event.y, &event.x, FALSE)) continue;
-				putlog("mouse %d %d",event.y, event.x);
+				if (event.y & 1)
+					event.x+=2;
+				if(event.x % 3 != 2)
+					continue;
+				{   int p = (7 - event.y) * 4 + event.x / 3;
+					int num = _popc(b & ALLONE(p));
+					putlog("%d num=%d", p, num);
+					if( (b & (1<<p)) == 0) { // New
+						b |= 1<<p;
+						insert_bit(w,num,pen==0 || pen==2);
+						insert_bit(d,num,pen>1);
+						goto new_doska;
+					}
+					// remove
+					b ^= 1 << p;
+					remove_bit(w,num);
+					remove_bit(d,num);
+					goto new_doska;
+				}
+				goto new_doska;
 		}
 	}
 	endwin();
--- facecontrol.h
+++ facecontrol.h
@@ -1,9 +1,10 @@
 PROCTYPE int FaceControl(uint32_t busy, uint32_t iwhite, uint32_t idamka) {
         int small = _popc(busy&0x0000000f);
         int big   = _popc(busy&0xf0000000);
+		int arank = _popc(busy);
 // least small bits
         if( ((~iwhite)&(~idamka)) & ((1<<small)-1) ) return -1;
 // last big bits
-        if( ((iwhite)&(~idamka)) >> ( RANK - big) ) return -1;
+        if( ((iwhite)&(~idamka)) >> ( arank - big) ) return -1;
         return 0;
 }
